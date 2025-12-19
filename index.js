9ff0bf596997404312a1ef33f60fd49bc22c6d06
Valentin FOURNY <fourny.valentin@gmail.com>
2025-12-20 00:40:58 +0100

    Add 01 courses
    Add 02 courses
    update module

--- index.js
+++
@@ -1,9 +0,0 @@
-import {add} from 'mathjs';
-import chalk from 'chalk';
-
-const a = 2;
-const b = 3;
-
-const result = add(a, b);
-
-console.log(chalk.green(`La somme de ${a} et ${b} est ${result}`));
